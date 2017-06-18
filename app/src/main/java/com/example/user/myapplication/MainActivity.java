5ea142e39415b2d10b9584753845cc9a9000ea00
fcu-d0280327 <d0280327@fcu.edu.tw>
2017-06-18 21:46:05 +0800

    Modify navigation button and check wifi is enable or not

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -4,12 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+//Wifi list
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    WifiManager wifi;
     private TextView mTextMessage;
+    private FrameLayout frameContent;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_list);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_more);
+                    frameContent.removeAllViews();
+                    checkWifi();
                     return true;
             }
             return false;
@@ -40,6 +70,60 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        frameContent = (FrameLayout) findViewById(R.id.content);
     }
 
+    private void checkWifi(){
+        wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if(wifi.isWifiEnabled() == false) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Remind");
+            dialog.setMessage("Your Wi-Fi is not enabled, enable?");
+            dialog.setIcon(android.R.drawable.ic_dialog_info);
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    wifi.setWifiEnabled(true);
+                    Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+                    frameContent.removeAllViews();
+                    showWifiList();
+                }
+            });
+            dialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    frameContent.removeAllViews();
+                    setRefreshImage();
+                }
+            });
+            dialog.show();
+        }else{
+            //TODO Start Scan Wifi
+        }
+    }
+
+    private void setRefreshImage(){
+        ImageButton ib = new ImageButton(this);
+        ib.setImageResource(R.drawable.icons8_refresh_48);
+        ib.setBackground(null);
+        ib.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkWifi();
+            }
+        });
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        lp.gravity = Gravity.CENTER;
+        frameContent.addView(ib, lp);
+    }
+
+    private void showWifiList(){
+        TextView wifiMessage = new TextView(this);
+        wifiMessage.setText("hi");
+        frameContent.addView(wifiMessage);
+    }
 }
