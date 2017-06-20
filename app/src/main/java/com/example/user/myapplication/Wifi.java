c2a5c5331e9d0991df223e53fc6d5559d17e61f8
Wei-Ying Xin <z96385274142@yahoo.com.tw>
2017-06-20 11:37:40 +0800

    firebase error

---
+++ app/src/main/java/com/example/user/myapplication/Wifi.java
@@ -0,0 +1,26 @@
+package com.example.user.myapplication;
+
+import android.graphics.Bitmap;
+
+public class Wifi {
+
+    private String addr;
+
+    private String agency;
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+}
